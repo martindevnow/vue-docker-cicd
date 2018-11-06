293df9718ddbc4a8895e9cf6247f5c73d9ab134e
Benjamin Martin <ben@martindevnow.com>
2018-11-06 13:02:13 -0500

    Fixed broken test and update build steps

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -7,6 +7,6 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg + 'fail');
+    expect(wrapper.text()).toMatch(msg);
   });
 });
