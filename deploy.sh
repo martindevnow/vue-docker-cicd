0f5949ab1a08790f0f1e28159137ef7eec919fd4
Benjamin Martin <ben@martindevnow.com>
2018-11-13 00:09:41 -0500

    Make a deploy script, needs testing

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+
+# TODO: Confirm script, upload to prod server to test
+
 set -e
 
 DOCKER_IMAGE=$1
 URL_SUBDOMAIN=$2
-CONAINER_NAME="vue-docker-cicd-container"
+CONAINER_NAME="vue-docker-cicd-${URL_SUBDOMAIN}"
 
-# TODO: Work on deploy script
+# Check for arguments
+if [[ $# -lt 1 ]] ; then
+    echo '[ERROR] You must supply a Docker Image to pull'
+    exit 1
+fi
+
+echo "Deploying Vue Docker CICD Project to Docker Container"
+
+#Check for running container & stop it before starting a new one
+if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
+    docker stop ${CONTAINER_NAME}
+fi
+
+echo "Starting Vue Docker CICD Project using Docker Image name: $DOCKER_IMAGE"
+
+docker run -e VIRTUAL_HOST=${URL_SUBDOMAIN}.martindevnow.com -d --rm=true --name ${CONTAINER_NAME} $DOCKER_IMAGE
+
+docker ps -a
