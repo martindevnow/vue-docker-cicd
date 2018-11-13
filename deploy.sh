47e876f0a9e13d3bffc49546db2fa6879c0255fe
Benjamin Martin <ben@martindevnow.com>
2018-11-13 13:00:46 -0500

    Fixed spelling mistake in Deploy script and added a new test runner in Package.json to avoid having more steps in docker file

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,22 @@
 
 set -e
 
-DOCKER_IMAGE="martindevnow/${1}"
-URL_SUBDOMAIN=$2
-CONTAINER_NAME="vue-docker-cicd-${URL_SUBDOMAIN}"
-
 # Check for arguments
 if [[ $# -lt 2 ]] ; then
     echo '[ERROR] You must supply a Docker Image to pull and Subdomain to Deploy'
     exit 1
 fi
 
+DOCKER_IMAGE="martindevnow/${1}"
+URL_SUBDOMAIN=$2
+
 if [[ $URL_SUBDOMAIN == "master" ]] ; then
     URL_SUBDOMAIN="qa"
-fi
-
-if [[ $URL_SUBDOMAIN == "develop" ]] ; then
+elif [[ $URL_SUBDOMAIN == "develop" ]] ; then
     URL_SUBDOMAIN="dev"
 fi
+
+CONTAINER_NAME="vue-docker-cicd-${URL_SUBDOMAIN}"
 
 echo "========"
 echo "Docker Image   = ${DOCKER_IMAGE}"
@@ -30,13 +29,12 @@ echo "URL Subdomain  = ${URL_SUBDOMAIN}"
 echo "Container Name = ${CONTAINER_NAME}"
 echo "========"
 
-
-
 echo "Stopping Existing Container with Same Name (If Applicable)"
 #Check for running container & stop it before starting a new one
-if [[ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]] ; then
+if [[ $(docker inspect -f '{{.State.Running}}' ${CONTAINER_NAME}) == "true" ]] ; then
     echo "... Stopping"
     docker stop ${CONTAINER_NAME}
+else echo "... container not running"
 fi
 
 echo "Starting Vue Docker CICD Project using Docker Image name: $DOCKER_IMAGE"
