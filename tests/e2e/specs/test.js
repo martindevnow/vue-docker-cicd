8226942f38eeeab606bf83ed675f2f55efe9e275
Benjamin Martin <ben@martindevnow.com>
2018-12-04 17:25:46 -0500

    Remove mock broken test to get green build

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -5,8 +5,4 @@ describe('My First Test', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
   })
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h2', 'Welcome to Your Vue.js + TypeScript App')
-  })
 })
