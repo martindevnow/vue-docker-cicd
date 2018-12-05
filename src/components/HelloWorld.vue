86916f9e2897cbb37749fe3dae0622d70d87f1a3
Benjamin Martin <ben@martindevnow.com>
2018-12-05 12:32:43 -0500

    Content Change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-<h2>Test!!!</h2>
+    <h2>Latest on Master v0.2.0</h2>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
